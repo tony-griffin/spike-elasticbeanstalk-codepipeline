7faaef9ac5de44e179d543305d65928c145101ec
Tony Griffin <tony@madetech.com>
2020-10-14 15:19:55 +0100

    Update app.js to server.js and express port to 8080

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send("Welcome to my server spike for Elastic Beanstalk!");
