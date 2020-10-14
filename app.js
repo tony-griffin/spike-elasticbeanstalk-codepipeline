21b530d6fcbba5740600c03b9fab1e0b7630dcdc
Tony Griffin <tony@madetech.com>
2020-10-14 14:04:13 +0100

    Add basic files for Express server to run on port 5001

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my server spike for Elastic Beanstalk!");
+});
+
+app.listen(port, () => console.log(`Server listening on ${port}`));
